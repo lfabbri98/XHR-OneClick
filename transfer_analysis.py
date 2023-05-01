59f912703c5dd1aa312a487a84f3ef033d3690ab
Luca <79851576+lfabbri98@users.noreply.github.com>
2023-05-01 18:38:57 +0200

    Creation of function to analyze transfer data

--- transfer_analysis.py
+++ transfer_analysis.py
@@ -1,6 +1,7 @@
 import classTransfer as ct
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def extract_transfer_data(data: ct.Transfer, Cox, W, L, Total_dose, regime = "linear"):
     """
@@ -12,7 +13,6 @@ def extract_transfer_data(data: ct.Transfer, Cox, W, L, Total_dose, regime = "li
     #Initialize threshold vector
     Vth = []
     Mu = []
-    ss = []
     times = []
 
     #Determine ID threshold for mobility and threshold voltage.
@@ -36,8 +36,34 @@ def extract_transfer_data(data: ct.Transfer, Cox, W, L, Total_dose, regime = "li
     #Calculate dose array
     dose = np.linspace(0,Total_dose, len(Vth))
 
+    #Return subthreshold slope
+    ss = extraction_subthreshold_slope(data)
+
     #Create the output dataframe
-    output = {"Time": times, "Dose": dose, "Vth": Vth, "Mobility":Mu }
+    output = {"Time": times, "Dose": dose, "Vth": Vth, "Mobility":Mu, "SS":ss }
     out = pd.DataFrame(output)
     return out
 
+def extraction_subthreshold_slope(data: ct.Transfer):
+    ss = []
+
+    #First calculate the fitting range to find subthreshold slope
+    #We can use the maximum slope as reference point and define a tolerance around it
+    for i,j in enumerate(data):
+        #Calculate first derivative
+        vg = j.VG
+        dvg = vg[1]-vg[0]
+        id_prime = abs(np.gradient(j.ID, dvg))
+        id2 = np.gradient(id_prime, dvg)
+        max_id = np.argmax(id2)
+
+        fig, ax = plt.subplots()
+        ax.plot(vg, id2)
+        plt.show()
+
+        vg_low = vg[max_id - int(5/100 * max_id)]
+        vg_high = vg[max_id + int(5/100 * max_id)]
+
+        ss.append(j.calculate_subthreshold(vg_low, vg_high))
+    
+    return ss
