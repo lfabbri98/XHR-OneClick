17d529ebcca5f50a0ebb302d6fad60b2794cfaf8
Luca <79851576+lfabbri98@users.noreply.github.com>
2023-05-02 12:24:06 +0200

    Main adaptation to plot transfer analysis and output file

--- main.py
+++ main.py
@@ -3,49 +3,61 @@ import pandas as pd
 import data_read as dr
 import matplotlib.pyplot as plt
 import transfer_analysis as ta
+import os
 
-"""
-names = ["VG","IG","tG","VD","ID","tD"]
-data = pd.read_csv("test_data.dat", sep="\t", names=names, header=1)
 
-t1 = ct.Transfer(data.VG, data.IG, data.VD, data.ID)
+def main(Irrad_path, Rec_path, Cox, W, L, Total_dose, outputbool, outputpath):
+    irrad = dr.read_folder(Irrad_path)
+    irrad_data = ta.extract_transfer_data(irrad, Cox, W, L, Total_dose)
 
-th = t1.calculate_threshold(0.9e-5)
-print(th)
+    #Figure 1: plot of transfers in linear scale and log scale
+    fig, (ax11, ax12)  = plt.subplots(1,2)
+    for i in irrad:
+        ax11.plot(i.VG, i.ID/1e-6)
+        ax12.plot(i.VG, i.ID)
+    ax11.set_xlabel("$V_G$ (V)")
+    ax11.set_ylabel("$I_D (\mu A)$")
+    ax11.set_title("Transfers in linear scale")
+    ax12.set_xlabel("$V_G$ (V)")
+    ax12.set_ylabel("$I_D$ (A)")
+    ax12.set_yscale("log")
+    ax12.set_title("Transfers in log scale")
 
-print(t1.calculate_mobility(0.9e-5, 1e-9, 4e-6, 8e-6))
-print(t1.calculate_subthreshold(-1,0))
+    fig.tight_layout()
 
-t1.plot_transfer()
-"""
+    #Figure 2: Plot of threshold variation in function of dose
+    fig2, ax2 = plt.subplots()
+    ax2.plot(irrad_data.Dose, irrad_data.Vth)
+    ax2.set_xlabel("Dose (Gy)")
+    ax2.set_ylabel("$V_{th}$ (V)")
+    ax2.set_title("Threshold in function of dose")
+    fig2.tight_layout()
 
-"""
-d = dr.read_file("test_recovery.dat")
-fig, ax = plt.subplots()
-for i in range(len(d)):
-    ax.plot(d[i].VG, d[i].ID)
-    print(d[i].time)
+    print("Threshold shift = ",irrad_data.Vth.values[-1]-irrad_data.Vth[0],"V")
 
-plt.show()
-"""
+    #Figure 3: Plot of mobility and subthreshold variation in function of dose
+    fig3, (ax31, ax32) = plt.subplots(1,2)
+    ax31.plot(irrad_data.Dose, irrad_data.Mobility)
+    ax32.plot(irrad_data.Dose, irrad_data.SS)
+    ax31.set_xlabel("Dose (Gy)")
+    ax31.set_ylabel("$\mu (cm^2 /Vs)$")
+    ax31.set_title("Mobility")
+    ax32.set_xlabel("Dose (Gy)")
+    ax32.set_ylabel("Subthreshold slope (V/dec)")
+    ax32.set_title("Subthreshold slope")
+    fig3.tight_layout()
 
-#To change is using mac or windows
-a = dr.read_folder("./Transfer/*.dat")
-vth = []
-time = []
+    #File ouput
+    #Creation of output directory, check if existent
+    directory_name = outputpath
 
-for i in range(len(a)):
-   vth.append(a[i].calculate_threshold(5e-5))
-   time.append((a[i].time-a[0].time).seconds/3600)
-   
-fig, ax = plt.subplots()
-for i in range(len(a)):
-    ax.plot(a[i].VG, a[i].ID)
+# Check if the directory already exists
+    if not os.path.exists(directory_name):
+    # If it does not exist, create it
+        os.makedirs(directory_name)
+    print("Directory", directory_name, "created.")
 
-dd = ta.extract_transfer_data(a, 3e-8, 4e-6, 8e-6, 800)
 
-fig2, ax2 = plt.subplots()
-ax2.plot(dd.Dose, dd.Mobility)
+    irrad_data.to_csv(directory_name+"Transfer_results.txt", sep="\t")
 
-plt.show()
-
+    plt.show()
