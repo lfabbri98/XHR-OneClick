523a1494128cc9b2e64b1a4079dfd8b9c8874c4f
Luca <79851576+lfabbri98@users.noreply.github.com>
2023-05-02 11:15:56 +0200

    Created configuration file and starting of the program

---
+++ start_analysis.py
@@ -0,0 +1,14 @@
+"""
+Use this file to configure and start the execution of XRH analysis.
+"""
+#------------------------
+# Configuration file 
+#------------------------
+
+configuration_file = "./config.txt"
+
+### DO NOT MODIFY UNDER THIS LINE
+
+#------------------------
+# Import libraries
+#------------------------
